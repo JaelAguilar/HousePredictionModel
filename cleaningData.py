74f86d6de66c81cd0240297c223348ee103ed31e
Jael Aguilar <cjaelaguilar@gmail.com>
2023-05-09 16:08:59 -0600

    Created functions to clean data in specific ways

--- cleaningData.py
+++ cleaningData.py
@@ -13,14 +13,30 @@ df = pd.read_csv('Data/newDataUpdated.tsv', sep='\t')
 #df.head(10)
 print(df.isnull().sum())
 
-# Baños
-df['Baños'] = df['Baños'].astype("float64")
-df['Baños'].fillna(round(df['Baños'].mean(),1),inplace=True)
 
-# Armarios empotrados
-df['Armarios empotrados'].fillna(False,inplace=True)
+def NaNtoMean(*attributeList):
+    global df
+    for attribute in attributeList:
+        df[attribute] = df[attribute].astype('float64')
+        df[attribute].fillna(round(df[attribute].mean(),1),inplace=True)
+    
+def NaNtoFalse(*attributeList):
+    global df
+    for attribute in attributeList:
+        df[attribute].fillna(False,inplace=True)
+        
+def deleteColumns(*attributeList):
+    global df
+    for attribute in attributeList:
+        df = df.drop(attribute, axis=1)
 
-print(df['Roof Garden'].unique())
+NaNtoMean('Baños','Estacionamientos','Construidos (m²)','Habitaciones (en total)','Recámaras','Terreno (m²)')
 
+NaNtoFalse('Armarios empotrados','Roof Garden','Totalmente cercado','Estacionamiento techado','Alberca','Jardín','Internet de banda ancha disponible','Estacionamiento para Visitas','Estacionamiento vigilado','Calefacción','Balcón','Acceso a TV de paga','Patio','Garaje','Jacuzzi','Estudio','Piso de loseta','Sistema de alarma','Cancha de tenis','Cocina Equipada','Cuarto de servicio','Gimnasio','Estacionamiento abierto','Área de juegos infantiles','Aire acondicionado','Terraza','Área de entretenimiento al aire libre','Intercomunicador','Chimenea','Alberca','Piso de duela')
 
-#print(df.isnull().sum())
+deleteColumns('Nivel','Mantenimiento','Disponible desde','Construido (Año)','Condiciones de Precio')
+
+# Special cases -> Amueblado, Superficie construida (m²)
+print('\n',df['Amueblado'].value_counts(dropna=False))
+
+print(df.isnull().sum())
