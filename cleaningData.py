6ad46dcfb8c970bb65033ad64775d8b72b31ae39
Jael Aguilar <cjaelaguilar@gmail.com>
2023-05-15 16:01:08 -0600

    Added special case for Habitaciones (en total)

--- cleaningData.py
+++ cleaningData.py
@@ -26,16 +26,20 @@ def deleteColumns(*attributeList):
     global df
     for attribute in attributeList:
         df = df.drop(attribute, axis=1)
-        
-#Special cases -> Superficie construida (m²), Amueblado
+
+#Special cases -> Superficie construida (m²), Amueblado, Habitaciones (en total)
 df['Construidos (m²)'] = np.where(df['Construidos (m²)'].isna(),df['Superficie construida (m²)'],df['Construidos (m²)'])
+
+df['Recámaras'] = np.where(df['Habitaciones (en total)'].isna()==False,df['Habitaciones (en total)'],df['Recámaras'])
+
 for old, new in [('Yes',True),('Sí',True),('No',False)]:
     df['Amueblado'] = np.where(df['Amueblado']==old,new,df['Amueblado'])
 
-NaNtoMean('Baños','Estacionamientos','Habitaciones (en total)','Recámaras','Terreno (m²)','Construidos (m²)')
+NaNtoMean('Baños','Estacionamientos','Recámaras','Terreno (m²)','Construidos (m²)')
 
 NaNtoFalse('Armarios empotrados','Roof Garden','Totalmente cercado','Estacionamiento techado','Alberca','Jardín','Internet de banda ancha disponible','Estacionamiento para Visitas','Estacionamiento vigilado','Calefacción','Balcón','Acceso a TV de paga','Patio','Garaje','Jacuzzi','Estudio','Piso de loseta','Sistema de alarma','Cancha de tenis','Cocina Equipada','Cuarto de servicio','Gimnasio','Estacionamiento abierto','Área de juegos infantiles','Aire acondicionado','Terraza','Área de entretenimiento al aire libre','Intercomunicador','Chimenea','Alberca','Piso de duela','Amueblado')
 
-deleteColumns('Nivel','Mantenimiento','Disponible desde','Construido (Año)','Condiciones de Precio','Superficie construida (m²)')
+
+deleteColumns('Nivel','Mantenimiento','Disponible desde','Construido (Año)','Condiciones de Precio','Superficie construida (m²)','Habitaciones (en total)')
 
 df.to_csv('Data/newCleanData.tsv',sep='\t',index=False)
