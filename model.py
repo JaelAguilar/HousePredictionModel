92570d1eeeee155d2f12985f6cea5dafc21d0289
Jael Aguilar <cjaelaguilar@gmail.com>
2023-05-21 21:03:04 -0600

    split and transform data

--- model.py
+++ model.py
@@ -1,19 +1,23 @@
-#Data analysis
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
 
 
-#from sklearn.preprocessing import StandardScaler #Standarization
-#from sklearn.ensemble import IsolationForest #Outlier detection
-
-#Modeling
-#from keras.models import Sequential
-#from keras.layers import Dense
-#from keras.callbacks import EarlyStopping
-#from keras.optimizers import Adam
-
-data = pd.read_csv("Data/newCleanData.tsv",sep='\t')
+data = pd.read_csv("Data/newCleanData2.tsv",sep='\t')
 print(data.head())
 print(data.describe())
 print("TIPO",type(data["Baños"]))
 
+# Split data between features and prices
+x = data.drop('precio',axis='columns').values
+y = data['precio'].values
+
+# Split data between training and testing sets by random
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2,random_state=42)
+
+# Scale the features
+# TODO: Compare betwwen MinMax and Standard scalers
+scaler = MinMaxScaler()
+x_train = scaler.fit_transform(x_train)
+x_test = scaler.fit_transform(x_test)
