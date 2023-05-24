22f40e30d020906f1fe144dacc47014391be6b00
Jael Aguilar <cjaelaguilar@gmail.com>
2023-05-24 13:58:53 -0600

    Finished model, but it gives me one error

--- model.py
+++ model.py
@@ -19,11 +19,13 @@ x = data.drop(['precio','titulo','direccion'],axis='columns')
 x_cat = LabelEncoder
 y = data['precio']
 
+categories = ['ciudad','Colonia']
+numerical_cols = len(data.columns)-5
 
 # Scale the numerical features
 # TODO: Compare betwwen MinMax and Standard scalers
 scaler = MinMaxScaler()
-numerical_x = scaler.fit_transform(x.drop(['ciudad','Colonia'],axis=1))
+numerical_x = scaler.fit_transform(x.drop(categories,axis=1))
 
 print(x)
 
@@ -38,10 +40,11 @@ print(x)
 
 # ============= CATEGORIES =================== #
 categorical_x = pd.DataFrame()
-categories = ['ciudad','Colonia']
+
 embedding_layers = []
-input_layers = []
+input_layers = [layers.Input(shape=(numerical_cols,))] #Added one initial that corresponds to ALL numerical values
 lblEncoders = {}
+
 for category in categories:
     # Label Encode all categories (giving them a value of 1 to n-1)
     categorical_x = LabelEncoder().fit_transform(x[category])
@@ -59,28 +62,35 @@ for category in categories:
     embedding_layers.append(embedding_layers)
     input_layers.append(input_layer)
     
-# Concatenate numerical and embedded features
+# Concatenate numerical and categorical input layers
+concatenated_Input = layers.Concatenate()(input_layers)
 
-# Code to use when adding more categorical data
-# concatenated = keras.layers.Concatenate()([city_embedded,suburb_embedded])
 
 # Using 2 hidden layers
 # TODO: Compare between using 1-3 hidden layers and changing units
+modelLayers = layers.Dense(64,activation='relu')(concatenated_Input)
+modelLayers = layers.Dense(64,activation='relu')(modelLayers)
 
-model = keras.Model()
+#Output layer
+modelLayers = layers.Dense(1)(modelLayers)
+
+#Creating the model
+model = keras.Model(inputs=concatenated_Input,outputs=modelLayers)
 
 #============= COMPILE =============== #
 # TODO: Test other compilers
-model.compile(optimizer=Adam, loss='mean_squared_error')
+model.compile(optimizer='adam', loss='mean_squared_error')
 
 # ============= TRAIN =============== #
 # Split data between training and testing sets by random
-x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2,random_state=42)
+x_train,x_test,y_train,y_test = train_test_split([*embedding_layers,numerical_x],y,test_size=0.2,random_state=42)
 
+#Training
 model.fit(x_train,y_train,epochs=5,batch_size=32,verbose=1)
 
 # ============ EVALUATE ================= #
 loss = model.evaluate(x_test,y_test,verbose=0)
+print("Loss: {loss}")
 
 # ============ PREDICTIONS =================== #
 predictions = model.predict(x_test)
