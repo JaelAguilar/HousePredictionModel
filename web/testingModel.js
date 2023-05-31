6d90a3fa229be2a11fdf4d3c4ec849b3e31bae38
Jael Aguilar <cjaelaguilar@gmail.com>
2023-05-30 18:44:11 -0600

    Added embedding and functions to normalize data

--- web/testingModel.js
+++ web/testingModel.js
@@ -11,11 +11,10 @@ const btn = document.getElementById("btn");
 //parsefloat es en los que ingresar
 //0 y 1 parseInt
 btn.onclick = () => {
-    let m2 = document.getElementById('m2').value;
-    let esta = document.getElementById('esta').value;
-    let m2construidos = document.getElementById('m2construidos').value;
-    let recam = document.getElementById('recam').value;
-    let terreno = document.getElementById('terreno').value;
+    let esta = document.getElementById('esta');
+    let m2construidos = document.getElementById('m2construidos');
+    let recam = document.getElementById('recam');
+    let terreno = document.getElementById('terreno');
     let pisoduela = document.getElementById('pisoduela').value;
     let armarios = document.getElementById('armarios').value;
     let jardint = document.getElementById('jardint').value;
@@ -47,29 +46,67 @@ btn.onclick = () => {
     let comunicador = document.getElementById('comunicador').value;
     let chimenea = document.getElementById('chimenea').value;
     let alberca = document.getElementById('alberca').value;
-
+    let baño = document.getElementById('baño');
+    
+    
 
     //tensor va en el modelo
     //tensor = entrada
     //let en vez de var
     //parsefloat es en los que ingresar
     //0 y 1 parseInt
-
     if (modelo != null) {
         let tensor = tf.tensor2d(
             [[
-                parseFloat(baño), parseInt(pisoduela), parseInt(armarios), parseInt(jardint), parseInt(cercado), parseFloat(esta), parseInt(estacionat),
-                parseFloat(m2construidos), parseInt(amueblado), parseFloat(recam), parseInt(jardin), parseInt(internetb), parseInt(estacionamientov), parseInt(calefaccion),
+                minmax(baño), parseInt(pisoduela), parseInt(armarios), parseInt(jardint), parseInt(cercado), minmax(esta), parseInt(estacionat),
+                minmax(m2construidos), parseInt(amueblado), minmax(recam), parseInt(jardin), parseInt(internetb), parseInt(estacionamientov), parseInt(calefaccion),
                 parseInt(balcon), parseInt(tv), parseInt(patio), parseInt(garage), parseInt(jacuzzi), parseInt(estudio), parseInt(pisolo),
                 parseInt(alarma), parseInt(tennis), parseInt(cocina), parseInt(cuartoservicio), parseInt(gym), parseInt(estacionamientoa), parseInt(juegos),
-                parseInt(clima), parseInt(terraza), parseInt(entretenimientolibre), parseInt(estacionamientovisita), parseInt(comunicador), parseInt(chimenea), parseFloat(terreno),
-                parseInt(alberca)
+                parseInt(clima), parseInt(terraza), parseInt(entretenimientolibre), parseInt(estacionamientovisita), parseInt(comunicador), parseInt(chimenea), minmax(terreno),
+                parseInt(alberca), ...embedding()
 
             ]])
+        console.log("TENSOR", tensor.arraySync())
         let prediccion = modelo.predict(tensor).dataSync();
-        prediccion = Math.round(prediccion, 2)
-        document.getElementById("resultado").innerHTML = "Precio: " + prediccion + "€/mes";
+        /*prediccion = Math.round(prediccion, 2)*/
+        document.getElementById("resultado").innerHTML = "Precio: $" + Math.round(prediccion/10);
     } else {
         document.getElementById("resultado").innerHTML = "Intenta de nuevo en un rato...";
     }
 }
+
+
+function embedding() {
+    let [colonia, ciudad] = address.split(', ')
+    /*console.log('col ',colonia,' ciu ',ciudad)*/
+    let colIndex = colonias.indexOf(colonia)+1
+    let ciuIndex = ciudades.indexOf(ciudad) + 1
+    let Indexes = [ciuIndex,colIndex]
+    /*console.log(colIndex, ciuIndex)*/
+    let embeddingData = JSON.parse(embeddedData)
+    /*console.log(embeddingData)
+    console.log(embeddingData['ciudad'])*/
+    let categoryTensor = []
+    let ind = 0
+    for (const category in embeddingData) {
+        let currentIndex = String(Indexes[ind])+'.0'
+        /*console.log(currentIndex)
+        console.log(embeddingData[category])*/
+        for (const e in embeddingData[category]) {
+            /*console.log(embeddingData[category][e][currentIndex])*/
+            categoryTensor.push(embeddingData[category][e][currentIndex])
+        }
+        ind+=1
+    }
+    /*console.log('Tensor',categoryTensor)*/
+    return categoryTensor
+    
+}
+    
+function minmax(html) {
+    value = parseFloat(html.value)
+    min = parseFloat(minmaxValues[html.id]['min'])
+    max = parseFloat(minmaxValues[html.id]['max'])
+    return (value-min)/(max-min)
+}
+    
